4529a0c63fc601e613cd20a0c39478f9efbc7bc2
Andrew Williams <andy@tensixtyone.com>
2017-12-15 17:37:38 +0000

    Produce output using PyYAML rather than print statements

--- scenegen.py
+++ scenegen.py
@@ -5,95 +5,101 @@ import configparser
 from urllib import parse
 
 import requests
+import yaml
 
-LIGHT_ATTRS = ["transition", "profile", "brightness", "flash"]
-LIGHT_COLOR_TYPES = ["xy_color", "rgb_color", "color_temp", "color_name"]
+__version__ = '0.0.1'
+
+LIGHT_ATTRS = ['transition', 'profile', 'brightness', 'flash']
+LIGHT_COLOR_TYPES = ['xy_color', 'rgb_color', 'color_temp', 'color_name']
 
 
 def error(message):
     sys.stderr.write("error: %s\n" % message)
     sys.exit(1)
 
-def get_states(url, key):
-    if key != None:
-        headers = {'x-ha-access': key}
-    else:
-        headers = {}
+def get_states(url, key=None):
+    """Get a dump of all current entities in Home Assistant"""
+    headers = {
+        'Accept': 'application/json',
+        'User-Agent': 'scenegen {}'.format(__version__),
+    }
+    if key:
+        headers['X-HA-Access'] = key
 
     apiurl = parse.urljoin(url, '/api/states')
-
     resp = requests.get(apiurl, headers=headers)
 
     if resp.status_code != 200:
         error("Error calling Home Assistant: {}, {}".format(resp.status_code, resp.reason))
-
     return resp.json()
 
 def output_attrs(state, args):
-    parts = state["entity_id"].split(".")
-    type = parts[0]
-    name = parts[1]
-    if type == "light" and "light" in args.types:
-        print ("  {}:".format(state["entity_id"]))
-        print ("    state: {}".format(state["state"]))
-    for attr in LIGHT_ATTRS:
-        if attr in state["attributes"]:
-            print("    {}: {}".format(attr, round(float(state["attributes"][attr]))))
-    for color_type in LIGHT_COLOR_TYPES:
-        if color_type in state["attributes"] and args.colortype == color_type:
-            print("    {}: {}".format(color_type, state["attributes"][color_type]))
+    """Filter an entity attributes for use with a Home Assistant scene"""
+    device_type = state['entity_id'].split('.')[0]
+    nustate = {}
 
-    if type == "switch" and "switch" in args.types:
-        print ("  {}:".format(state["entity_id"]))
-        print ("    state: {}".format(state["state"]))
+    # Light
+    if device_type in args.types:
+        if device_type == 'light':
+            nustate['state'] = state['state']
+            # Copy required attributes
+            for attr in LIGHT_ATTRS:
+                if attr in state['attributes']:
+                    nustate[attr] = state['attributes'][attr]
+            # Add in color type state if set
+            if args.colortype in state['attributes']:
+                nustate[args.colortype] = state['attributes'][args.colortype]
+        # Switch
+        elif device_type == 'switch':
+            nustate = state['state']
+        else:
+            error('Unsupported device type ({}) detected while trying to process entity {}'.format(device_type, state['entity_id']))
+        return {state['entity_id']: nustate}
 
 def main():
     # Get command line args
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("url", help="url for Home Assistant instance")
-    parser.add_argument("-k", "--key", help="API Key of Home Assistant instance")
-    parser.add_argument("-s", "--scenename", help="Name of scene to generate", default = "My New Scene")
-    parser.add_argument("-m", "--mapfile", help="Name of mapfile to enable device filtering")
-    parser.add_argument("-f", "--filter", help="Comma separated list of device collections as defined in mapfile")
-    parser.add_argument("-c", "--colortype", help="color type to use", default = "color_temp", choices = LIGHT_COLOR_TYPES)
-    parser.add_argument("-t", "--types", help="list of device types to include", default = "light,switch")
+    parser.add_argument('url', help='url for Home Assistant instance')
+    parser.add_argument('-k', '--key', help='API Key of Home Assistant instance')
+    parser.add_argument('-s', '--scenename', help='Name of scene to generate', default='My New Scene')
+    parser.add_argument('-m', '--mapfile', help='Name of mapfile to enable device filtering')
+    parser.add_argument('-f', '--filter', help='Comma separated list of device collections as defined in mapfile')
+    parser.add_argument('-c', '--colortype', help='color type to use', default='color_temp', choices=LIGHT_COLOR_TYPES)
+    parser.add_argument('-t', '--types', help='list of device types to include', default='light,switch')
     args = parser.parse_args()
 
-    devices = {}
-    if args.mapfile:
-        config = configparser.ConfigParser()
-        config.read_file(open(args.mapfile))
-        for section in config.sections():
-            devices[section] = {}
-            for option in config.options(section):
-                devices[section][option] = config.get(section, option)
+    filter_list = []
+    if args.mapfile and args.filter:
+        filters = args.filter.split(',')
 
-    filters = []
-    if args.filter:
-        if args.mapfile:
-            filters = args.filter.split(",")
-        else:
-            error("Must specify a mapfile if using filters")
+        # Load in the mapfile, any sections that match a filter, load the option keys into the filter list
+        config = configparser.ConfigParser()
+        with open(args.mapfile, 'r') as fobj:
+            config.read_file(fobj)
+        for section in config.sections():
+            if section in filters:
+                filter_list.extend(config.options(section))
+
     try:
         states = get_states(args.url, args.key)
-        print("name: {}".format(args.scenename))
-        print("entities:")
-        for state in states:
-            if args.mapfile:
-                if args.filter:
-                    for filter in filters:
-                        if filter in devices and state["entity_id"] in devices[filter]:
-                            output_attrs(state, args)
-                        else:
-                            for section in devices:
-                                if state["entity_id"] in devices[section]:
-                                    output_attrs(state, args)
-            else:
-                output_attrs(state, args)
-    except:
-        raise
+    except requests.exceptions.RequestException as exc:
+        error('Unknown error occured while trying to read the state from Home Assistant: {}'.format(str(exc)))
 
-if __name__ == "__main__":
+    # Iterate all entities and produce a scene
+    output = {'entities': {}, 'name': args.scenename}
+    for state in states:
+        if args.mapfile and args.filter:
+            # If we're provided with a map file and filter list, exclude the match entities
+            if state['entity_id'] not in filter_list:
+                continue
+        entity_state = output_attrs(state, args)
+        if entity_state:
+            output['entities'].update(entity_state)
+
+    # Write the resulting YAML to stdout
+    sys.stdout.write(yaml.dump(output, default_flow_style=False))
+
+if __name__ == '__main__':
     main()
